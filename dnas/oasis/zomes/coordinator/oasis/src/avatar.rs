37a0137933635c62b2bdcb6f5c98c170c59daf4e
David Ellams <davidellams@hotmail.com>
2024-09-01 14:33:40 -0400

    Fixed bugs in get_avatar_by_username in avarar.rs

--- dnas/oasis/zomes/coordinator/oasis/src/avatar.rs
+++ dnas/oasis/zomes/coordinator/oasis/src/avatar.rs
@@ -1,5 +1,7 @@
 use hdk::prelude::*;
+
 use oasis_integrity::*;
+
 #[hdk_extern]
 pub fn create_avatar(avatar: Avatar) -> ExternResult<Record> {
     let avatar_action_hash = create_entry(&EntryTypes::Avatar(avatar.clone()))?;
@@ -50,15 +52,18 @@ pub fn get_avatar(original_avatar_hash: ActionHash) -> ExternResult<Option<Recor
 pub fn get_avatar_by_username(username: String) -> ExternResult<Option<Record>> {
     
     //TODO: Need to search/lookup the avatar or actionhash which matches the given username...
+    let path = Path::from(username);
+    let links = get_links(AnyLinkableHash::try_from(path.path_entry_hash()?)?, LinkTypes::AllAvatarsByUsername, None);
     
-    let links = get_links(username.clone(), LinkTypes::AllAvatarsByUsername, None)?;
     let latest_link = links
         .into_iter()
         .max_by(|link_a, link_b| link_a.timestamp.cmp(&link_b.timestamp));
-    let latest_avatar_hash = match latest_link {
+    
+        let latest_avatar_hash = match latest_link {
         Some(link) => ActionHash::from(link.target.clone()),
-        None => username.clone(),
+        None => path.path_entry_hash(),
     };
+
     get(latest_avatar_hash, GetOptions::default())
 }
 
